367ec09fbf147fdfdd00bee2e840be8dab8eed0b
cmunozcad-sys <cmunozcad@gmail.com>
2025-11-25 13:28:09 +0100

    prueba de FUNCIONES CON DOS

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Random sc = new Random(200);
     }
 }
