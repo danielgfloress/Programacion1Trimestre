d9a521de26c969cc22e43f92b8bc377404a50320
daniel <danielgarciaf07@gmail.com>
2025-11-25 14:08:21 +0100

    reinicio

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,4 @@
-import java.util.Random;
 
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        Random sc = new Random(200);
-    }
-    public static void prueba(){
-        System.out.println("Hello World");
-        Scanner sc = new Scanner(System.in);
-    }
+
 }
