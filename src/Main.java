c35e83b9796118e9bffc67e59d8f4e124162a466
daniel <danielgarciaf07@gmail.com>
2025-11-25 13:26:37 +0100

    prueba con funciones

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+    }
+    public static void prueba(){
+        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
     }
 }
