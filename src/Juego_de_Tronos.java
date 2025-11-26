14f61a4cddd9a2d8ddb499196bcb3a00e96ac49e
cmunozcad-sys <cmunozcad@gmail.com>
2025-11-26 13:18:02 +0100

    funcion para que salga lento el texto

--- src/Juego_de_Tronos.java
+++ src/Juego_de_Tronos.java
@@ -14,7 +14,7 @@ public class Juego_de_Tronos {
         int opciones;
 
 
-        System.out.println("\nComienza la aventura y tienes que hacerte con el trono de hierro, consigue el personaje que más te guste e intenta hacerte con el trono de hierro superando todos los peligros que aparecen por el camino.\n");
+        imprimirLento("\nComienza la aventura y tienes que hacerte con el trono de hierro, consigue el personaje que más te guste e intenta hacerte con el trono de hierro superando todos los peligros que aparecen por el camino.\n", 10);
         System.out.println(YELLOW + "Elige un personaje: " + RESET);
         System.out.println(GREEN + "1. Jon Nieve" + RESET);
         System.out.println(RED + "2. Daenerys Targaryen" + RESET);
@@ -26,11 +26,11 @@ public class Juego_de_Tronos {
             int opciones1;
 
 
-            System.out.println("\n\n\nHas elegido a Jon Nieve, te encuantras en El Muro y posees:\n");
+            imprimirLento("\n\n\nHas elegido a Jon Nieve, te encuantras en El Muro y posees:\n", 10);
             System.out.println(RED + "ESPADA DE JON NIEVE" + RESET);
             System.out.println(BLUE + "LOBO BLANCO (FANTASMA)" + RESET);
             System.out.println(GREEN + "EJÉRCITO DEL NORTE" + RESET);
-            System.out.println("\n\nAvanza con tus refuerzos, pero antes debes escoger por donde quieres ir.\n ");
+            imprimirLento("\n\nAvanza con tus refuerzos, pero antes debes escoger por donde quieres ir.\n ", 10);
             System.out.println(RED + "1. Roca Dragón" + RESET);
             System.out.println(YELLOW + "2. Nido de Águilas" + RESET);
             System.out.println(BLUE + "3. Invernalia" + RESET);
@@ -50,11 +50,11 @@ public class Juego_de_Tronos {
 
             int opciones2;
 
-            System.out.println("\n\n\nHas elegido a Daenerys Tragaryen, te encuentras en Qarth y posees:\n ");
+            imprimirLento("\n\n\nHas elegido a Daenerys Tragaryen, te encuentras en Qarth y posees:\n ", 10);
             System.out.println(RED + "3 DRAGONES" + RESET);
             System.out.println(BLUE + "EJÉRCITO DE ESCLAVOS" + RESET);
             System.out.println(GREEN + "JORAH (GURDAESPALDAS)" + RESET);
-            System.out.println("\n\nAvanza con tus refuerzos, pero antes debes escoger por donde quieres ir.\n ");
+            imprimirLento("\n\nAvanza con tus refuerzos, pero antes debes escoger por donde quieres ir.\n ", 10);
             System.out.println(RED + "1. Bahia de los esclavos" + RESET);
             System.out.println(YELLOW + "2. Yunkai " + RESET);
             System.out.println(BLUE + "3. Astapor" + RESET);
@@ -73,11 +73,11 @@ public class Juego_de_Tronos {
 
             int opciones3;
 
-            System.out.println("\n\n\nHas elegido al Rey de la Noche, te encuentras al norte de El Muro y posees:\n ");
+            imprimirLento("\n\n\nHas elegido al Rey de la Noche, te encuentras al norte de El Muro y posees:\n ",10);
             System.out.println(BLUE + "LANZA CONGELADA" + RESET);
             System.out.println(RED + "PODER DE RESURRECCIÓN" + RESET);
             System.out.println(GREEN + "DRAGÓN REVIVIDO" + RESET);
-            System.out.println("\n\nAvanza con tus refuerzos, pero antes debes escoger por donde quieres ir.\n ");
+            imprimirLento("\n\nAvanza con tus refuerzos, pero antes debes escoger por donde quieres ir.\n ", 10);
             System.out.println(BLUE + "1. El Muro" + RESET);
             System.out.println(YELLOW + "2. Pueblo de los salvajes " + RESET);
             System.out.println(RED + "3. Casa Craster" + RESET);
@@ -92,4 +92,18 @@ public class Juego_de_Tronos {
         }
 
     }
+
+    public static void imprimirLento(String texto, int velocidad) {
+        for (int i = 0; i < texto.length(); i++) {
+            System.out.print(texto.charAt(i));
+            try {
+                Thread.sleep(velocidad);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(); // Salto de línea al final
+    }
+
+
 }
